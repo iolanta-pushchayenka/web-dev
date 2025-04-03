b1b8583be9ac89b53c73244581c2ca67fdf27a1f
lana <ialanta.pushchayenka@student.ehu.lt>
2025-04-03 16:04:01 +0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 function App() {
+  const items = ['Phone1', 'Phone2', 'Phone3'];
 
   return (
     <div>
-      Дзякуй мір
+      <ul>
+        {items.map((phone) => (
+          <li>{phone}</li>
+        ))}
+      </ul>
     </div>
   );
 }
