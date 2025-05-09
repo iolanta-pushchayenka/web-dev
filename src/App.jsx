04ccaa7397637a530cab2f0f3403d3c3bd522bbd
lana <ialanta.pushchayenka@student.ehu.lt>
2025-05-09 13:04:24 +0300

    Added homework5

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import MenuPage from './pages/MenuPage';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <MenuPage />;
+  return <HomePage />;
+ // return <MenuPage />;
 }
 
 export default App;
